a2030ab37ed2f09ef727cac34019a6c2d45735b4
jcameron10 <jovonnec@gmail.com>
2021-06-09 12:38:15 -0400

    created folders and started course

---
+++ start/firstFile.js
@@ -0,0 +1,5 @@
+console.log("hello world")
+//brings the same result as global.console.log
+
+//Global objects 
+//contains all objects, values and methods we can use in node.js w/o importing functionality
---
+++ start/global.js
@@ -0,0 +1,6 @@
+//require- imports  modules (other js files containing code, npm modules or modules that come with the installation of node)  )
+const path = require('path') //path is a module from node that helps us work with path strings
+
+//basename function plucks the filename  from global filename variable
+console.log(__dirname) //gives directory name
+console.log(__filename) //gives full name and path to current file
