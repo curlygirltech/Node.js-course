087d7f5aebdc0fcab1988e2e1635d01e03f435d6
jcameron10 <jovonnec@gmail.com>
2021-06-09 17:59:07 -0400

    Node Modules

---
+++ start/core.js
@@ -0,0 +1,29 @@
+//Core Modules
+
+const path = require("path")
+
+// const dirUploads = path.join(__dirname, "www", "files", "uploads")
+// //using path.join allows us to reach modules in another folder and put them together. 
+
+// console.log(dirUploads)//gives full path of modules from a different folder
+
+const util = require("util")
+
+util.log(path.basename(__filename)) //gives us current name of file
+util.log(" ^ the name of the current file ")
+//the difference between util.log and console.log is that you have to require (see line 10) the util module gives us the date and timestamp
+
+const v8 = require("v8") //v8 module
+util.log(v8.getHeapStatistics()) // returns an object of available memory
+
+//DESCTRUCTURING MODULES//
+//we can desstructure out of our require statements
+
+const { log } = require("util")
+const { getHeapStatistics } = require("v8") //v8 module
+
+log(getHeapStatistics())
+//one con of using this is that you have to look at all the require statements to know where the log and get heapstatistics come from 
+
+
+
--- start/timers.js
+++ start/timers.js
@@ -5,13 +5,26 @@ let currentTime = 0
 
 const incTime = () => {
   currentTime += waitInterval // current time incrememnted by waitinterval
-  console.log(`waiting ${currentTime/1000} seconds`)
+  const p = Math.floor((currentTime / waitTime) * 100)
+  //Math.floor gives us a rounded # when we get a decimal. here, we want a percentage
+  process.stdout.clearLine()//clears the last line
+  process.stdout.cursorTo(0)//puts cursor at beginning of line
+  process.stdout.write(`waiting ...${p}%`)
+
 }
 console.log(`setting a ${waitTime / 1000} second delay`)//waitTime / 1000 gives the time in seconds
 
 //create a function for when the timer is finished 
-const timerFinihed = () => console.log("done")// when the timer is finished we are going to console.log "done"
+const timerFinihed = () => {
+  clearInterval(interval)//remember to clear interval or this will run indefinitely! set a variable for interval 
+  process.stdout.clearLine()
+  process.stdout.cursorTo(0)
+  console.log("done")
+}// when the timer is finished we are going to console.log "done"
 
+const interval = setInterval(incTime, waitInterval) //1st argument is a func, next argument is time that interval should wait
+//this will call the incTime function over and over (ever half sec) 
 setTimeout(timerFinihed, waitTime)//1st argument is what to do when the timer is finished, 2nd arguement is the amount of time 
 //this will run asynchronously for 3 secs
+
 
